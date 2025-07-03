c7be37f773caa7f19ec4cbb731367fac0a4c7a3c
Amir4ik <amir4ik11@gmail.com>
2025-07-04 00:59:22 +0400

    1) Added ApplicationComponent

--- app/src/main/java/ru/otus/daggerhomework/ApplicationComponent.kt
+++ app/src/main/java/ru/otus/daggerhomework/ApplicationComponent.kt
@@ -1,4 +1,20 @@
 package ru.otus.daggerhomework
 
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Qualifier
+
+@Component(modules = [])
 interface ApplicationComponent {
+    @Component.Factory
+    interface ApplicationComponentFactory {
+        fun create(@BindsInstance @ApplicationContext context: Context): ApplicationComponent
+    }
+
+    @ApplicationContext
+    fun applicationContext(): Context
 }
+
+@Qualifier
+annotation class ApplicationContext
